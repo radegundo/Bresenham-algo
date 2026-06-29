71e1de71762a92732caea80f1f0c7b2ce8d381ea
radegundo <rade.salvador@gmail.com>
2026-06-30 00:09:36 +0200

    Not really working but getting there

--- src/logic.rs
+++ src/logic.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ math::VectorSpace, prelude::* };
 use bevy_grid::Grid;
 
 #[derive(Component)]
@@ -15,10 +15,33 @@ pub fn get_line(
     if let Ok(p0) = start_point_query.single() && let Ok(p1) = end_point_query.single() {
         let (x0, y0) = (p0.translation.x as i32, p0.translation.y as i32);
         let (x1, y1) = (p1.translation.x as i32, p1.translation.y as i32);
-        let dx = x1 - x0;
-        let dy = y1 - y0;
-        let p = 2 * dy - dx;
-        let grid_coords = grid.get_grid_coords(Vec3::new(x0 as f32, y0 as f32, 0.0));
-        println!("{}", grid_coords.unwrap());
+        // let p = 2 * dy - dx;
+        let start = grid
+            .get_grid_coords(Vec3::new(x0 as f32, y0 as f32, 0.0))
+            .unwrap_or(Vec3::ZERO);
+        let end = grid.get_grid_coords(Vec3::new(x0 as f32, y0 as f32, 0.0)).unwrap_or(Vec3::ZERO);
+        let dx = (end.x as i32) - (start.x as i32);
+        let dy = (end.y as i32) - (start.y as i32);
+
+        let (mut x0, mut y0) = (start.x as i32, start.y as i32);
+        let (mut x1, mut y1) = (end.x as i32, end.y as i32);
+
+        let mut line = vec![];
+
+        if dx != 0 {
+            let m = dy / dx;
+            let mut y = y0;
+
+            for i in 0..dx {
+                let pd = m * i + y0;
+                if pd - y > y + 1 - pd {
+                    line.push(Vec2::new(i as f32, (y as f32) + 1.0));
+                    y = y + 1;
+                } else {
+                    line.push(Vec2::new(i as f32, y as f32));
+                }
+                println!("{}, {}", i, y);
+            }
+        }
     }
 }

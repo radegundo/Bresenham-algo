b306c681bce3aabd1513ad1f8d439b5bc1f7ee1d
radegundo <rade.salvador@gmail.com>
2026-06-30 13:12:28 +0200

    First octant working

--- src/logic.rs
+++ src/logic.rs
@@ -10,32 +10,35 @@ pub struct EndPoint;
 pub fn get_line(
     grid: Res<Grid>,
     start_point_query: Query<&Transform, With<StartPoint>>,
-    end_point_query: Query<&Transform, With<EndPoint>>
+    end_point_query: Query<&Transform, With<EndPoint>>,
+    mut gizmos: Gizmos
 ) {
     if let Ok(p0) = start_point_query.single() && let Ok(p1) = end_point_query.single() {
         let (x0, y0) = (p0.translation.x as i32, p0.translation.y as i32);
         let (x1, y1) = (p1.translation.x as i32, p1.translation.y as i32);
         // let p = 2 * dy - dx;
         let start = grid
-            .get_grid_coords(Vec3::new(x0 as f32, y0 as f32, 0.0))
+            .get_grid_coords(&Vec3::new(x0 as f32, y0 as f32, 0.0))
             .unwrap_or(Vec3::ZERO);
-        let end = grid.get_grid_coords(Vec3::new(x1 as f32, y1 as f32, 0.0)).unwrap_or(Vec3::ZERO);
+        let end = grid.get_grid_coords(&Vec3::new(x1 as f32, y1 as f32, 0.0)).unwrap_or(Vec3::ZERO);
         let dx = (end.x as i32) - (start.x as i32);
         let dy = (end.y as i32) - (start.y as i32);
 
         let (mut x0, mut y0) = (start.x as i32, start.y as i32);
         let (mut x1, mut y1) = (end.x as i32, end.y as i32);
 
-        let mut line = vec![];
+        let mut line: Vec<Vec2> = vec![];
 
         if dx != 0 {
             let m = (dy as f32) / (dx as f32);
             let mut y = y0;
             let mut x = x0;
 
-            for i in 0..dx + 1 {
-                let pd = m * (x as f32) + (y0 as f32);
-                if pd - (y as f32) > (y as f32) + 1.0 - pd {
+            for _ in 0..dx + 1 {
+                let py = m * (x as f32) + (y0 as f32);
+                let d0 = py - (y as f32);
+                let d1 = (y as f32) + 1.0 - py;
+                if d1 < d0 {
                     line.push(Vec2::new(x as f32, (y as f32) + 1.0));
                     y += 1;
                 } else {
@@ -43,7 +46,15 @@ pub fn get_line(
                 }
                 x += 1;
             }
-            println!("{:?}", line);
+            for tile in line {
+                if let Some(world_pos) = grid.get_world_coords(&tile.extend(0.0)) {
+                    gizmos.circle_2d(
+                        Isometry2d::from_xy(world_pos.x, world_pos.y),
+                        10.0,
+                        Color::WHITE
+                    );
+                }
+            }
         }
     }
 }
